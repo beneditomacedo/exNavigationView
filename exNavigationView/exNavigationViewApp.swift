7d22ba2b48986b0a83f56b1d127a587c2796c7e3
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 21:26:21 -0300

    Initial Commit

---
+++ exNavigationView/exNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  exNavigationViewApp.swift
+//  exNavigationView
+//
+//  Created by Benedito Macedo on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct exNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
